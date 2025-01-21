5a29552c0efca7c449abe18f246672480e04d479
Prashant <prashantsherkar7515400@gmail.com>
2025-01-21 14:31:25 +0530

    dates changed

--- app/dates/page.tsx
+++ app/dates/page.tsx
@@ -7,20 +7,22 @@ function page() {
 
     <div className=' my-[10vh] text-center m-7'></div>
     <div className='  text-center duration-1000 text-3xl md:my-[400px] flex flex-col gap-9 items-center justify-center '>
-      <div className='flex items-center justify-center flex-col gap-3' >
+      <div className='flex items-center justify-center   col gap-3' >
 
-          Paper Submission deadline: 1st May, 2025 
-          <br />
+          <div>
+
+          Paper Submission deadline: 1<sup>st</sup> May, 2025
+          </div>
       </div>
       <div>
 
 
-Acceptance Notification: 30th June, 2025 
+          Acceptance Notification: 30<sup>th</sup> June, 2025
       </div>
       <div>
 
 
-Registration/Camera Ready Submission: 20th July, 2025 
+Registration/Camera Ready Submission: 15<sup>th</sup> July, 2025
       </div>
 
     </div>
